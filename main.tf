b7b2baf45e881a96b7d1151f6db5c9b7e60ace4a
adrianocanofre <adrianoknofre@gmail.com>
2020-03-29 19:36:54 -0300

    refactoring in subnet resource

--- main.tf
+++ main.tf
@@ -11,58 +11,12 @@ resource "aws_vpc" "this" {
     }
 }
 
-resource "aws_subnet" "public_1" {
-
+resource "aws_subnet" "public" {
+  count            = length(var.cidr_public_subnet)
   vpc_id                          = aws_vpc.this.id
-  cidr_block                      = "10.0.1.0/24"
+  cidr_block                      = element(var.cidr_public_subnet, count.index)
   map_public_ip_on_launch = true
   tags ={
-      "Name" = format("%s-pub-1", var.name)
+      "Name" = format("%s-pub-%d", var.name, count.index+1)
     }
-}
-
-resource "aws_subnet" "public_2" {
-
-  vpc_id                          = aws_vpc.this.id
-  cidr_block                      = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  tags ={
-      "Name" = format("%s-pub-2", var.name)
-    }
-}
-
-resource "aws_subnet" "public_3" {
-
-  vpc_id                          = aws_vpc.this.id
-  cidr_block                      = "10.0.3.0/24"
-  map_public_ip_on_launch = true
-  tags ={
-      "Name" = format("%s-pub-3", var.name)
-    }
-}
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "cidr" {
   type    = string
   default = "10.0.0.0/20"
 }
+
+variable "cidr_public_subnet" {
+  type = list
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
