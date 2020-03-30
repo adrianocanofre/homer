5d4ca30a51dfc96b9276e7589e734ead66b8831b
adrianocanofre <adrianoknofre@gmail.com>
2020-03-29 21:12:16 -0300

    add private subnet and az

--- main.tf
+++ main.tf
@@ -7,16 +7,31 @@ provider "aws" {
 resource "aws_vpc" "this" {
   cidr_block = var.cidr
   tags ={
-      "Name" = format("%s", var.name)
+      "Name" = format("%s", var.vpc_name)
     }
 }
 
 resource "aws_subnet" "public" {
-  count            = length(var.cidr_public_subnet)
-  vpc_id                          = aws_vpc.this.id
-  cidr_block                      = element(var.cidr_public_subnet, count.index)
+  count                   = length(var.cidr_public_subnet)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = element(var.cidr_public_subnet, count.index)
+  availability_zone       = element(var.azs, count.index)
   map_public_ip_on_launch = true
   tags ={
-      "Name" = format("%s-pub-%d", var.name, count.index+1)
+      "Name" = format("%s-pub-%d", var.vpc_name, count.index+1)
+    }
+}
+
+
+resource "aws_subnet" "private" {
+  count                   = length(var.cidr_private_subnet)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = element(var.cidr_private_subnet, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = false
+  tags ={
+      "Name" = format("%s-pub-%d", var.vpc_name, count.index+1)
     }
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,7 @@
-variable "name" {
+variable "vpc_name" {
   type    = string
   default = "vpc-dev"
 }
-
 
 variable "cidr" {
   type    = string
@@ -10,6 +9,11 @@ variable "cidr" {
 }
 
 variable "cidr_public_subnet" {
-  type = list
+  type    = list
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+  type    = list
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
