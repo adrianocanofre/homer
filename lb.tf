2ce285ca3c5643a5db4d8041fdb67b22c53b25e3
adrianocanofre <adrianoknofre@gmail.com>
2020-04-05 20:03:08 -0300

    add target group

--- lb.tf
+++ lb.tf
@@ -1,8 +1,7 @@
 resource "aws_lb" "this" {
 
-  name        = "teste"
-
-  load_balancer_type = "application"
+  name               = local.lb_name
+  load_balancer_type = var.lb_type
   internal           = false
   subnets            = aws_subnet.public.*.id
   security_groups    = [aws_security_group.allow_tls.id]
@@ -15,8 +14,8 @@ resource "aws_lb" "this" {
 
 resource "aws_lb_listener" "this" {
   load_balancer_arn = aws_lb.this.arn
-  port     = 80
-  protocol = "HTTP"
+  port              = var.http_port
+  protocol          = var.http_protocol
 
   default_action {
     type = "fixed-response"
@@ -26,5 +25,22 @@ resource "aws_lb_listener" "this" {
       message_body = "HEALTHY"
       status_code  = "200"
     }
+  }
+}
+
+
+resource "aws_lb_target_group" "main" {
+
+  name        = local.tg_name
+
+  vpc_id      = aws_vpc.this.id
+  port        = var.http_port
+  protocol    = var.http_protocol
+
+
+  depends_on = [aws_lb.this]
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
--- locals.tf
+++ locals.tf
@@ -2,4 +2,5 @@ locals {
   env      = lookup(var.envs, terraform.workspace)
   vpc_name = format("vpc-%s", local.env)
   lb_name  = format("lb-%s", local.env)
+  tg_name  = format("tg-%s", local.env)
 }
