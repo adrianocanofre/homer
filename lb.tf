b3378ad35406b5ef608a8d91c3ed7cccc4be4a04
adrianocanofre <adrianoknofre@gmail.com>
2020-04-05 17:57:43 -0300

    create minimal lb

--- lb.tf
+++ lb.tf
@@ -5,7 +5,10 @@ resource "aws_lb" "this" {
   load_balancer_type = "application"
   internal           = false
   subnets            = aws_subnet.private.*.id
+  security_groups    = [aws_security_group.allow_tls.id]
 
-
-
+  lifecycle {
+    create_before_destroy = true
+  }
+  
 }
