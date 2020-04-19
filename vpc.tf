4178f207056bd74e30e1e35ef19f06125e9f2737
adrianocanofre <adrianoknofre@gmail.com>
2020-04-19 16:56:40 -0300

    add nat gateway

--- vpc.tf
+++ vpc.tf
@@ -63,3 +63,42 @@ resource "aws_route_table_association" "public" {
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+      "Name" = format("%s-prvt-rt", local.vpc_name)
+    }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.cidr_private_subnet)
+
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = var.all_cidr
+  nat_gateway_id         = aws_nat_gateway.this.id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_eip" "this" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "this" {
+
+  allocation_id = aws_eip.this.id
+  subnet_id = aws_subnet.public.0.id
+
+
+  depends_on = [aws_internet_gateway.this]
+}
