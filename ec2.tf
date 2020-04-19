2e9995d60669fb95e9b8fa93615fca197c4c9b54
adrianocanofre <adrianoknofre@gmail.com>
2020-04-19 16:57:45 -0300

    change sub pub to privt

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "nginx" {
+	ami = "ami-07ebfd5b3428b6f4d"
+	instance_type = "t2.nano"
+	key_name = var.key_par
+	user_data = file("files/install_nginx.sh")
+	subnet_id = aws_subnet.private[1].id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+}
