94ea775a7eaff3dd1f2d424bd348f02b0dca5837
adrianocanofre <adrianoknofre@gmail.com>
2020-04-25 10:55:52 -0300

    provider

--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = var.region
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "aws-terraform"
 }
 
 terraform {
   backend "s3" {
-    # Bucket config!
     bucket   = "homer-s3-state"
     key      = "global/s3/terraform.tfstate"
-    region   = "us-east-1"
+    region   = var.region
     profile  = "aws-terraform"
 
    # DynamoDB table config!
